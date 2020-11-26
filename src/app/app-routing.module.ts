98582fdfcd2d831aacd6dff0725f6bbeaa386893
Cristian Gutierrez <cdgutierrez456@gmail.com>
2020-11-26 10:45:15 -0500

    seccion3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ProductoComponent } from './pages/producto/producto.component';
 
-const routes: Routes = [];
+const app_routes: Routes = [
+
+  {path: 'home', component: CatalogoComponent},
+  {path: 'nosotros', component: NosotrosComponent},
+  {path: 'producto', component: ProductoComponent},
+  {path: '**',pathMatch: 'full', redirectTo: 'home'}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(app_routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ProductoComponent } from './pages/producto/producto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CatalogoComponent
+    CatalogoComponent,
+    NosotrosComponent,
+    ProductoComponent
   ],
   imports: [
     BrowserModule,

5f88c3324613efa3cfe4bc3342005c0775675919
Cristian Gutierrez <cdgutierrez456@gmail.com>
2020-11-27 21:17:35 -0500

    seccion6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -3,23 +3,36 @@ import { Injectable } from '@angular/core';
 import { InfoPagina } from '../interfaces/info.pagina.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InfoPaginaService {
-
-  info: InfoPagina = {}
+  info: InfoPagina = {};
   cargada = false;
 
-  constructor(private http: HttpClient) { 
-    console.log("pagina hecha putos")
+  equipo: any[] = []
 
-    //leer archivo JSON
-    this.http.get('assets/data/data-pagina.json')
-        .subscribe((resp: InfoPagina) =>{
-          this.cargada = true
-          this.info = resp
-          console.log(resp)
-        })
-
+  constructor(private http: HttpClient) {
+    this.cargarInfo()
+    this.cargarEquipo()
   }
+
+  private cargarInfo() {
+    //leer archivo JSON
+    this.http
+      .get('assets/data/data-pagina.json')
+      .subscribe((resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+      });
+  }
+
+  private cargarEquipo(){
+    //leer archivo JSON
+    this.http
+      .get('https://angular-html-8641a.firebaseio.com/equipo.json')
+      .subscribe((resp: any[]) => {
+        this.equipo = resp;
+      });
+  }
+
 }
